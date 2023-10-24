ad750b22c356ae6eb1533b9f9537836a42543f32
Rafael Yudi <rafael.tanaka@unesp.br>
2023-10-24 19:33:55 -0300

    change automating time

--- src/main/java/br/com/rafaelyudi/todoList/task/EmailSenderService.java
+++ src/main/java/br/com/rafaelyudi/todoList/task/EmailSenderService.java
@@ -27,7 +27,7 @@ public class EmailSenderService {
     private IUserRepository userRepository;
 
 
-    @Scheduled(initialDelay = 100000, fixedRate = 5000)
+    @Scheduled(cron = "0 0 18 * * ?")
     public void taskEmailSenderService(){
         var tasks = this.taskService.findTasksCloseEnd();
         
