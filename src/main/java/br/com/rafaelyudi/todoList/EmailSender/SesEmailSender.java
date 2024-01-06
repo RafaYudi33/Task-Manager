67e6d20cfc4f7af4479472219e248fabcdca9da8
RafaYudi33 <rafael.tanaka@unesp.br>
2024-01-06 20:19:08 -0300

    remove warnings

--- src/main/java/br/com/rafaelyudi/todoList/EmailSender/SesEmailSender.java
+++ src/main/java/br/com/rafaelyudi/todoList/EmailSender/SesEmailSender.java
@@ -1,5 +1,4 @@
 package br.com.rafaelyudi.todoList.EmailSender;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.AmazonServiceException;
