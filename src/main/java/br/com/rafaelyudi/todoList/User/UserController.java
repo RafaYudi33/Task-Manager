e417da8e36990e5277ced1fd2317f04ef783f576
RafaYudi33 <rafael.tanaka@unesp.br>
2024-01-19 13:42:39 -0300

    add consumes and produces in controllers

--- src/main/java/br/com/rafaelyudi/todoList/User/UserController.java
+++ src/main/java/br/com/rafaelyudi/todoList/User/UserController.java
@@ -2,6 +2,7 @@ package br.com.rafaelyudi.todoList.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO userData) { 
 
         var userCreated = this.userService.userCreate(userData);
