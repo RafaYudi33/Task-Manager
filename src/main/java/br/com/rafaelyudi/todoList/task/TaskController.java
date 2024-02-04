ea87b740628ee38b64b465505fb746273f98884f
RafaYudi33 <rafael.tanaka@unesp.br>
2024-02-04 13:51:50 -0300

    refactoring swagger annotations in create task method

--- src/main/java/br/com/rafaelyudi/todoList/task/TaskController.java
+++ src/main/java/br/com/rafaelyudi/todoList/task/TaskController.java
@@ -1,33 +1,23 @@
 package br.com.rafaelyudi.todoList.Task;
 
 
-import java.util.List;
-import java.util.UUID;
-
-
+import br.com.rafaelyudi.todoList.Errors.CustomResponseError;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
@@ -56,12 +46,13 @@ public class TaskController {
                                     @Content(mediaType = "application/xml", schema = @Schema(implementation = TaskDTO.class))
                             }
                     ),
-                    @ApiResponse(description = "Invalid date", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+                    @ApiResponse(description = "Invalid date", responseCode = "400", content = @Content(schema = @Schema(implementation = CustomResponseError.class))),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content(schema = @Schema(implementation = CustomResponseError.class))),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content(schema = @Schema(implementation = CustomResponseError.class)))
             }
     )
-    public ResponseEntity<TaskDTO> create(@Validated @RequestBody TaskDTO taskDto, HttpServletRequest request){
+
+    public ResponseEntity<TaskDTO> create(@RequestBody TaskDTO taskDto, HttpServletRequest request){
        var taskCreated = this.taskService.createTask(taskDto, request);
        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);     
     }
