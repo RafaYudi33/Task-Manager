e5f7bc90f420a02e9693473acb1e6e78198aa493
RafaYudi33 <rafael.tanaka@unesp.br>
2024-02-05 16:18:25 -0300

    add swagger annotations to create a user

---
+++ src/main/java/br/com/rafaelyudi/todoList/Config/Swagger/UserExampleRequestBody.java
@@ -0,0 +1,8 @@
+package br.com.rafaelyudi.todoList.Config.Swagger;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record UserExampleRequestBody(String username, String name, String password, String email){
+
+}
--- src/main/java/br/com/rafaelyudi/todoList/User/UserController.java
+++ src/main/java/br/com/rafaelyudi/todoList/User/UserController.java
@@ -1,5 +1,15 @@
 package br.com.rafaelyudi.todoList.User;
 
+import br.com.rafaelyudi.todoList.Config.Swagger.UserExampleRequestBody;
+import br.com.rafaelyudi.todoList.Errors.CustomResponseError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,16 +21,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users/v1")
+@Tag(name = "User", description = "Endpoints to managing users")
+@SecurityScheme(name = "Basic Auth", type = SecuritySchemeType.HTTP, scheme = "basic")
 public class UserController {
-
 
     @Autowired
     private UserService userService;
 
     @PostMapping(value = "/", consumes ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
                  produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userData) { 
-
+    @Operation(
+            summary = "Create a user",
+            description = "Create a user by passing JSON or XML",
+            tags = "User",
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "201", content = {
+                            @Content(schema = @Schema(implementation = UserDTO.class)),
+                            @Content(schema = @Schema(implementation = UserDTO.class))
+                    }),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponseError.class)),
+                            @Content(mediaType = "application/xml", schema = @Schema(implementation = CustomResponseError.class))
+                    }),
+                    @ApiResponse(description = "NotFound", responseCode = "404", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponseError.class)),
+                            @Content(mediaType = "application/xml", schema = @Schema(implementation = CustomResponseError.class))
+                    }),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponseError.class)),
+                            @Content(mediaType = "application/xml", schema = @Schema(implementation = CustomResponseError.class))
+                    }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponseError.class)),
+                            @Content(mediaType = "application/xml", schema = @Schema(implementation = CustomResponseError.class))
+                    })
+            }
+    )
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = UserExampleRequestBody.class)),
+                @Content(mediaType = "application/json", schema = @Schema(implementation = UserExampleRequestBody.class))
+        }
+    )
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userData) {
         var userCreated = this.userService.userCreate(userData);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
