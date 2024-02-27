ccfe6f49cb4c61c899ebc9bb7b5c71eb4358bd75
RafaYudi33 <rafael.tanaka@unesp.br>
2024-02-27 16:39:30 -0300

    add some constants

--- src/test/java/br/com/rafaelyudi/todoList/config/TestConfig.java
+++ src/test/java/br/com/rafaelyudi/todoList/config/TestConfig.java
@@ -2,4 +2,10 @@ package br.com.rafaelyudi.todoList.config;
 
 public class TestConfig {
     public static final int SERVER_PORT = 8888;
+    public static  final String HEADER_PARAM_ORIGIN = "origin"; 
+    public static final String HEADER_PARAM_AUTHORIZATION = "authorization";
+    
+    public static final String ALLOWED_DOMAIN = "http://localhost:4200";
+    public static final String NOT_ALLOWED_DOMAIN = "http://randomdomain";
+
 }
