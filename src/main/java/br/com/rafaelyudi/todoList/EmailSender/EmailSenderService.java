4690d2fd7f08a124f0bac70b1fe90a48d1aa77ac
RafaYudi33 <rafael.tanaka@unesp.br>
2024-03-19 16:58:11 -0300

    fix emailSender

--- src/main/java/br/com/rafaelyudi/todoList/EmailSender/EmailSenderService.java
+++ src/main/java/br/com/rafaelyudi/todoList/EmailSender/EmailSenderService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import br.com.rafaelyudi.todoList.EmailSender.SesEmailSender;
 import br.com.rafaelyudi.todoList.User.IUserRepository;
 
 @Component
