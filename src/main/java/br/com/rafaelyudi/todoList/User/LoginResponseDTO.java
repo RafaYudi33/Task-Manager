6175bdf43d204605338c32d7d4ed2a1ecb791d74
RafaYudi33 <rafael.tanaka@unesp.br>
2024-05-21 22:51:50 -0300

    fix: LoginResponseDTO

--- src/main/java/br/com/rafaelyudi/todoList/User/LoginResponseDTO.java
+++ src/main/java/br/com/rafaelyudi/todoList/User/LoginResponseDTO.java
@@ -1,7 +1,10 @@
 package br.com.rafaelyudi.todoList.User;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record LoginResponseDTO(String token, UUID idUser) {
+public record LoginResponseDTO(String token, LocalDateTime expirationTime) {
 
 }
