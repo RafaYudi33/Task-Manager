4025c5b3a18e22fdd28ccba6d51b1262c9428ef9
RafaYudi33 <rafael.tanaka@unesp.br>
2024-05-23 21:26:51 -0300

    fix: SwaggerConfig

--- src/main/java/br/com/rafaelyudi/todoList/Config/Swagger/SwaggerConfig.java
+++ src/main/java/br/com/rafaelyudi/todoList/Config/Swagger/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package br.com.rafaelyudi.todoList.Config.Swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +19,11 @@ public class SwaggerConfig {
                 .title("Task Manager")
                 .description("An api to manage tasks")
                 .version("Version 1")
-                .contact(new Contact().email("yudirafael33@gmail.com")));
-
+                .contact(new Contact().email("yudirafael33@gmail.com")))
+                .components(new Components().addSecuritySchemes("Bearer Authentication",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"));
     }
 
 
