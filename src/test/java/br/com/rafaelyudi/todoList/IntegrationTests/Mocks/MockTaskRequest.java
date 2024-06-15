ff8ca06dd8b8e144ebb3f5ba5e365aa5ffd50265
RafaYudi33 <rafael.tanaka@unesp.br>
2024-06-15 16:46:39 -0300

    fix: change mocks after add spring security

--- src/test/java/br/com/rafaelyudi/todoList/IntegrationTests/Mocks/MockTaskRequest.java
+++ src/test/java/br/com/rafaelyudi/todoList/IntegrationTests/Mocks/MockTaskRequest.java
@@ -19,11 +19,9 @@ import static io.restassured.RestAssured.given;
 public class MockTaskRequest {
     private static final LocalDateTime startAt =  LocalDateTime.of(2030,1,30,10,0);
     private static final LocalDateTime endAt = LocalDateTime.of(2030,3,30,10,0);
-    private static final ObjectMapper objectMapper = ObjectMapperConfig.configureObjectMapper();
 
-    public List<TaskDTO> mockPostListTasks(int taskNumberToMock, UserDTO userOwner) throws JsonProcessingException {
+    public List<TaskDTO> mockPostListTasks(int taskNumberToMock, UserDTO userOwner, String token,ObjectMapper objectMapper) throws JsonProcessingException {
         List<TaskDTO> listOfTasksPersisted = new ArrayList<>();
-
 
         for (int i = 0; i < taskNumberToMock; i++) {
            TaskDTO task = new TaskDTO(null,
@@ -39,6 +37,7 @@ public class MockTaskRequest {
             var specification = new RequestSpecBuilder()
                     .setPort(TestConfig.SERVER_PORT)
                     .addHeader(TestConfig.HEADER_PARAM_ORIGIN, TestConfig.ALLOWED_DOMAIN)
+                    .addHeader(TestConfig.HEADER_PARAM_AUTHORIZATION, "Bearer " + token)
                     .setContentType(TestConfig.MEDIA_TYPE_JSON)
                     .setBasePath(TestConfig.basePathTask)
                     .setBody(task)
@@ -48,15 +47,13 @@ public class MockTaskRequest {
 
             var content = given()
                     .spec(specification)
-                    .auth().preemptive().basic(userOwner.getUsername(), userOwner.getPassword())
-                    .post()
+                    .post("/")
                     .then()
                     .extract()
                     .body()
                     .asString();
 
             listOfTasksPersisted.add(i,objectMapper.readValue(content, TaskDTO.class));
-            System.out.println("mocks:"+task.getTitle());
         }
         return listOfTasksPersisted;
     }
