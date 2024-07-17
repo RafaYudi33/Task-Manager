d0799e23b1d5c8f502947eb1a18e986d9720e969
RafaYudi33 <rafael.tanaka@unesp.br>
2024-07-17 15:27:56 -0300

    fix: change name

--- src/main/java/br/com/rafaelyudi/todoList/TaskManagerApplication.java
+++ src/main/java/br/com/rafaelyudi/todoList/TaskManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    TodoListApplication {
+public class TaskManagerApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(TodoListApplication.class, args);
+		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 }
 		
